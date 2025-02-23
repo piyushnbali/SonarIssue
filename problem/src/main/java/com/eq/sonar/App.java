d4328f84427d2c8d644564ef84fb163fc34c3852
Piyush Bali <pnbali.4141@gmail.com>
2025-02-23 22:21:12 +0530

    Added files

---
+++ problem/src/main/java/com/eq/sonar/App.java
@@ -0,0 +1,13 @@
+package com.eq.sonar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
